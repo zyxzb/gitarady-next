d6373ba4edbf13e15653fdcb52f9ffd840760f4c
Bartek Z <bartekzarzyk@gmail.com>
2024-09-17 16:09:37 +0200

    add srcDoc attribute to speed up page loading

--- app/_components/organisms/Videos.tsx
+++ app/_components/organisms/Videos.tsx
@@ -26,6 +26,7 @@ const Videos = () => {
           aria-hidden='true'
           tabIndex={-1}
           loading='lazy'
+          srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/7JvjzwH_Q6o/?autoplay=1><img src=https://img.youtube.com/vi/7JvjzwH_Q6o/hqdefault.jpg alt='Gitarady, lekcje gry na gitarze- Shape of You by Ed Sheeran in 4 Steps'><span>▶</span></a>`}
         ></iframe>
         <iframe
           src='https://www.youtube.com/embed/UJ9eGN6SHFM'
@@ -35,6 +36,7 @@ const Videos = () => {
           aria-hidden='true'
           tabIndex={-1}
           loading='lazy'
+          srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/UJ9eGN6SHFM/?autoplay=1><img src=https://img.youtube.com/vi/UJ9eGN6SHFM/hqdefault.jpg alt='Gitarady, lekcje gry na gitarze - Improwizacja Am'><span>▶</span></a>`}
         ></iframe>
       </div>
     </Wrapper>
